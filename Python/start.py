e0c58844c4fb5f5d5914554da6dfdf1f7a8d4277
Ivan Hryhorutsa <ivanhrygorutsa@gmail.com>
2023-06-23 20:00:26 +0200

    README.md filling

---
+++ Python/start.py
@@ -0,0 +1,23 @@
+# write some comments
+'''write
+some
+comments'''
+
+#print("hello world")
+
+print("================INT===============")
+x=10
+#print(x,type(x))
+print(f"Value : {x}\nType: {type(x)}")
+
+print("================FLOAT===============")
+x=3.34
+print(f"Value : {x}\nType: {type(x)}")
+
+print("================STRING===============")
+x="Hello string"
+print(f"Value : {x}\nType: {type(x)}")
+
+print("================BOOLEAN===============")
+x=True
+print(f"Value : {x}\nType: {type(x)}")
---
+++ Python/task1.py
@@ -0,0 +1,24 @@
+# User input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Calculate sum and product
+sum_of_numbers = num1 + num2 + num3
+product_of_numbers = num1 * num2 * num3
+
+# Display the results
+print("Sum of the numbers:", sum_of_numbers)
+print("Product of the numbers:", product_of_numbers)
+
+
+# User input
+salary = float(input("Enter the monthly salary: "))
+loan_payment = float(input("Enter the monthly loan payment: "))
+utilities_debt = float(input("Enter the debt for utilities: "))
+
+# Calculate the remaining amount
+remaining_amount = salary - loan_payment - utilities_debt
+
+# Display the result
+print("Remaining amount:", remaining_amount)
